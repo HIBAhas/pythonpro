cbf1eb09ca6f3e1797e8cc7f94802945b5ec8dd9
HIBAhas <128007272+HIBAhas@users.noreply.github.com>
2023-03-16 02:41:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+"""Module pour calculer la puissance d'un nombre"""
+
 def calculer_puissance(nombre, puissance):
-    # Calculer la puissance
-    resultat = nombre ** puissance
-    return resultat
+    """Calcule la puissance d'un nombre.
+    Args:
+        nombre (float ou int): Le nombre à élever à la puissance.
+        puissance (float ou int): La puissance à laquelle élever le nombre.
+    Returns:
+        float ou int: Le résultat de la puissance.
+    """
+    return nombre ** puissance
